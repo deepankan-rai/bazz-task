54c6ffb0cf5ad65231c00cd57c56b0d0c615f3c4
@deepankan1 <deepankan@ramailo.tech>
2024-01-13 12:22:08 +0530

    more final commit

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = 3000;
-
+const cors = require('cors');
+app.use(cors());
 // const uri='mongodb+srv://deepankanrai2000:Deepankan@123@cluster0.hlfs9lo.mongodb.net/bazz?retryWrites=true&w=majority';
 app.use(bodyParser.json());
 const uri='mongodb+srv://deepankanrai2000:Deepankan123@cluster0.hlfs9lo.mongodb.net/?retryWrites=true&w=majority'
